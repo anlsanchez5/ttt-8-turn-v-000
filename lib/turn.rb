a923319e3c995f7b9ed2dbe215b1d081d571def7
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-08-23 18:40:08 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -16,3 +16,21 @@ def valid_move?(board, index)
   end
 end
     
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+def input_to_index(number)
+  integer = number.to_i 
+  index = integer - 1 
+end
+
+def move(board, index, char = "X")
+  board[index] = char
+  board 
+end 
