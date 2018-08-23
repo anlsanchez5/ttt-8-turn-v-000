6b6aafa35d1ed8a752ecd2326f82e7e9061d3d56
anlsanchez@ucdavis.edu <anlsanchez@ucdavis.edu>
2018-08-23 18:37:23 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,18 @@
+def position_taken?(arr,ind)
+    check = arr[ind]
+    if check == " " || check == "" || check == nil
+       false 
+    elsif check == "X" || check == "O"
+       true
+    end
+end
+
+
+def valid_move?(board, index)
+  if index.between?(0,8) && position_taken?(board,index) == false;
+    true
+  else
+    false
+  end
+end
+    
